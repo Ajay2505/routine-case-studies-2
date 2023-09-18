f5d14d14633e5b2d44cc712a646b5fc0ef9e597a
sudhamsh99 <92301797+sudhamsh99@users.noreply.github.com>
2023-09-18 15:41:42 +0530

    ds

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+function Marquee(selector, speed) {
+  const parentSelector = document.querySelector(selector);
+  const clone = parentSelector.innerHTML;
+  const firstElement = parentSelector.children[0];
+  let i = 0;
+  console.log(firstElement);
+  parentSelector.insertAdjacentHTML("beforeend", clone);
+  parentSelector.insertAdjacentHTML("beforeend", clone);
+
+  setInterval(function () {
+    firstElement.style.marginLeft = `-${i}px`;
+    if (i > firstElement.clientWidth) {
+      i = 0;
+    }
+    i = i + speed;
+  }, 0);
+}
+
+//after window is completed load
+//1 class selector for marquee
+//2 marquee speed 0.2
+window.addEventListener("load", Marquee(".marquee", 0.2));
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('body',{
+    backgroundColor: '#ffffff',
+    scrollTrigger: {
+        trigger: ".banner_section",
+        start: "20% top",
+        end: '70% top',
+        pin: false,
+        pinSpacing: true,
+        markers: false,
+        id: 'pin',
+        scrub:0
+    }
+});
+
+
